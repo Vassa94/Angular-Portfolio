5b314ad027adc1a80c0fc53f18b1d54c8c0bc910
Vassa94 <vassallo1994@gmail.com>
2022-08-07 15:02:24 -0300

    cambios menores CSS

--- src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sobre-mi',
@@ -8,12 +9,39 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
 })
 export class SobreMiComponent implements OnInit {
   miInfo:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  closeResult: any;
+  constructor(private datosPortfolio:PortfolioService,private modalService:NgbModal) { }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data =>{
       this.miInfo=data.about;
     })
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  openLg(content) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
