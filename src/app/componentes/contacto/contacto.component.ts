a2746869493eb5d6f74a02f04fa4d666e2610a73
Vassa94 <vassallo1994gmail.com>
2022-10-30 15:47:04 -0300

    Form contacto

--- src/app/componentes/contacto/contacto.component.ts
+++ src/app/componentes/contacto/contacto.component.ts
@@ -1,28 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.css'],
 })
 export class ContactoComponent implements OnInit {
-
   FormData: FormGroup;
 
-  constructor(private builder: FormBuilder) { 
+  constructor(private builder: FormBuilder, private contact: PortfolioService) {
     this.FormData = this.builder.group({
       Fullname: new FormControl('', [Validators.required]),
-      Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
-      Comment: new FormControl('', [Validators.required])
-      })
+      Email: new FormControl('', [
+        Validators.compose([Validators.required, Validators.email]),
+      ]),
+      Comment: new FormControl('', [Validators.required]),
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(id) {
+    console.log(FormData);
+    this.contact.contactMsg(FormData)
+      .subscribe(
+      (response) => {
+        location.href = 'https://mailthis.to/confirm';
+        console.log(response);
+      },
+      (error) => {
+        console.warn(error.responseText);
+        console.log({ error });
+      }
+    );
   }
-
-  onSubmit(id){
-
-  }
-
 }
--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -1,13 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-
-
 export class PortfolioService {
   private _refresh$ = new Subject<void>();
 
@@ -17,68 +15,59 @@ export class PortfolioService {
     return this._refresh$;
   }
 
-  api = 'https://portfoliobackendvassallo.herokuapp.com'
+  api = 'https://portfoliobackendvassallo.herokuapp.com';
+  apiContacto = 'https://mailthis.to/vassalloignacio';
 
   obtenerDatos(): Observable<any> {
     return this.http.get('./assets/data/data.json');
   }
 
   getHeader(): Observable<any> {
-    return this.http.get(this.api+'/personas/traer/1');
+    return this.http.get(this.api + '/personas/traer/1');
   }
 
   putHeader(body): Observable<object> {
-    return this.http
-      .put<any>(this.api+'/personas/editar/1', body)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-        })
-      );
+    return this.http.put<any>(this.api + '/personas/editar/1', body).pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
+    );
   }
 
   getEduc(): Observable<any> {
-    return this.http
-      .get(this.api+'/educacion/traer')
-      
+    return this.http.get(this.api + '/educacion/traer');
   }
 
   deleteEduc(id): Observable<object> {
-    return this.http
-      .delete(this.api+'/educacion/borrar/' + id)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-        })
-      );
+    return this.http.delete(this.api + '/educacion/borrar/' + id).pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
+    );
   }
 
   postEduc(body): Observable<any> {
-    return this.http
-      .post(this.api+'/educacion/crear', body)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-        })
+    return this.http.post(this.api + '/educacion/crear', body).pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
     );
   }
 
   putEduc(id, body): Observable<object> {
-    return this.http
-      .put<any>(this.api+'/educacion/editar/' + id, body)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-        })
-      );
+    return this.http.put<any>(this.api + '/educacion/editar/' + id, body).pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
+    );
   }
 
   getExp(): Observable<any> {
-    return this.http.get(this.api+'/experiencia/traer');
+    return this.http.get(this.api + '/experiencia/traer');
   }
 
   postExp(body): Observable<any> {
-    return this.http.post(this.api+'/experiencia/crear', body).pipe(
+    return this.http.post(this.api + '/experiencia/crear', body).pipe(
       tap(() => {
         this._refresh$.next();
         console.log('obserbable iniciado');
@@ -88,7 +77,7 @@ export class PortfolioService {
 
   putExp(id, body): Observable<object> {
     return this.http
-      .put<any>(this.api+'/experiencia/editar/' + id, body)
+      .put<any>(this.api + '/experiencia/editar/' + id, body)
       .pipe(
         tap(() => {
           this._refresh$.next();
@@ -98,27 +87,20 @@ export class PortfolioService {
   }
 
   deleteExp(id): Observable<object> {
-    return this.http
-      .delete(this.api+'/experiencia/borrar/' + id)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-          console.log('obserbable iniciado');
-        })
-      );
+    return this.http.delete(this.api + '/experiencia/borrar/' + id).pipe(
+      tap(() => {
+        this._refresh$.next();
+        console.log('obserbable iniciado');
+      })
+    );
   }
 
- 
-
   getProyec(): Observable<any> {
-    return this.http.get(this.api+'/proyectos/traer')
-    
+    return this.http.get(this.api + '/proyectos/traer');
   }
 
   postProyect(body): Observable<any> {
-    return this.http
-    .post(this.api+'/proyectos/crear', body)
-    .pipe(
+    return this.http.post(this.api + '/proyectos/crear', body).pipe(
       tap(() => {
         this._refresh$.next();
       })
@@ -126,35 +108,28 @@ export class PortfolioService {
   }
 
   putProyect(id, body): Observable<object> {
-    return this.http
-      .put<any>(this.api+'/proyectos/editar/' + id, body)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-          console.log('obserbable iniciado');
-        })
-      );
+    return this.http.put<any>(this.api + '/proyectos/editar/' + id, body).pipe(
+      tap(() => {
+        this._refresh$.next();
+        console.log('obserbable iniciado');
+      })
+    );
   }
 
   deleteProyect(id): Observable<object> {
-    return this.http
-      .delete(this.api+'/proyectos/borrar/' + id)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-        })
-      );
+    return this.http.delete(this.api + '/proyectos/borrar/' + id).pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
+    );
   }
 
-
   getSkills(): Observable<any> {
-    return this.http.get(this.api+'/habilidades/traer');
+    return this.http.get(this.api + '/habilidades/traer');
   }
 
   postSkill(body): Observable<any> {
-    return this.http
-    .post(this.api+'/habilidades/crear', body)
-    .pipe(
+    return this.http.post(this.api + '/habilidades/crear', body).pipe(
       tap(() => {
         this._refresh$.next();
       })
@@ -162,8 +137,7 @@ export class PortfolioService {
   }
 
   putSkill(id, body): Observable<object> {
-    return this.http
-      .put<any>(this.api+'/habilidades/editar/' + id, body)
+    return this.http.put<any>(this.api + '/habilidades/editar/' + id, body)
       .pipe(
         tap(() => {
           this._refresh$.next();
@@ -172,14 +146,14 @@ export class PortfolioService {
   }
 
   deleteSkill(id): Observable<object> {
-    return this.http
-      .delete(this.api+'/habilidades/borrar/' + id)
-      .pipe(
-        tap(() => {
-          this._refresh$.next();
-        })
-      );
+    return this.http.delete(this.api + '/habilidades/borrar/' + id).pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
+    );
   }
 
-  
+  contactMsg(input: any) {
+    return this.http.post(this.apiContacto, input);
+  }
 }
